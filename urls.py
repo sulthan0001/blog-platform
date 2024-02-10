d5e47a415188ff4cb72ccc737a209ba1869d4178
sulthan0001 <149578687+sulthan0001@users.noreply.github.com>
2024-02-10 10:52:03 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,27 +1,16 @@
 # -*- coding: utf-8 -*-
-from django.contrib.auth.decorators import login_required
-from django.urls import path
-
-from blog import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
 
 urlpatterns = [
-    path("", views.IndexView.as_view(), name="index"),
-    path("post/", views.PostListView.as_view(), name="post_list"),
-    path("post/<slug>/", views.PostDetailView.as_view(), name="post_detail"),
-    path("search/", views.SearchView.as_view(), name="search"),
-    path(
-        "posts/create/",
-        login_required(views.PostCreateView.as_view()),
-        name="post_create",
-    ),
-    path(
-        "posts/update/<slug>/",
-        login_required(views.PostUpdateView.as_view()),
-        name="post_update",
-    ),
-    path(
-        "posts/delete/<slug>/",
-        login_required(views.PostDeleteView.as_view()),
-        name="post_delete",
-    ),
+    path("admin/", admin.site.urls),
+    path("", include("blog.urls")),
+    path("", include("accounts.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
+
+if settings.DEBUG:  # pragma:no cover
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
